669675892c343666af2036e3c5960e0c180433c7
Lia Mason <lia.k.mason@gmail.com>
2020-11-29 04:23:49 -0500

    add headshot

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import Lia from "../images/about-photo.jpeg"
+import Lia2 from "../images/headshot.png"
 import aboutStyles from "./about.module.scss"
 
 const About = () => {
     return (
       <Layout>
-        <h1>About Page</h1>
+        <h1>About me</h1>
         <img src={Lia} alt="Headshot" className={aboutStyles.headshot}></img>
+        <img src={Lia2} alt="Headshot" className={aboutStyles.headshot}></img>
         <p>I'm in my third year of computer science at Queen's University, specializing in software design.
           I'm most interested in front-end development, UX & UI design, and technology for social impact!
         </p>
