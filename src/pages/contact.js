28e79381d091b93030a9595f4563f83f3b9c6d94
Lia Mason <lia.k.mason@gmail.com>
2020-12-02 19:17:35 -0500

    add social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,10 @@ import React from "react"
 import Layout from "../components/layout"
 import contactStyles from "./contact.module.scss"
 import Coffee from "../images/coffee.png"
+import Pinterest from "../images/pinterest.svg"
+import LinkedIn from "../images/linkedin-icon.svg"
+import Github from "../images/github.svg"
+import Mail from "../images/mail.svg"
 
 const Contact = () => {
   return (
@@ -11,7 +15,21 @@ const Contact = () => {
         <img src={Coffee} alt="Coffee" className={contactStyles.coffee}/>
       </div>
       <hr className={contactStyles.underline}/>
-      <p className={contactStyles.text}>Reach out to me to chat about anything :)</p>
+      <div className={contactStyles.textContainer}>
+        <p className={contactStyles.text}>Reach out to me to chat about anything :)</p>
+        <a href="mailto:lia.k.mason@gmail.com">
+        <img src={Mail} alt="Email" className={contactStyles.icon}></img>
+        </a>
+        <a href="https://www.linkedin.com/in/lia-k-mason/">
+        <img src={LinkedIn} alt="Linked In" className={contactStyles.icon}></img>
+        </a>
+        <a href="https://github.com/lia-mason">
+        <img src={Github} alt="Github" className={contactStyles.icon}></img>
+        </a>
+        <a href="https://www.pinterest.ca/liakmason/">
+        <img src={Pinterest} alt="Pinterest" className={contactStyles.icon}></img>
+        </a>
+      </div>
       <div></div>
     </Layout>
   )
