15202ca72739001a5639d37bcbc34274857f0a15
Lia Mason <lia.k.mason@gmail.com>
2020-12-09 19:24:04 -0500

    edit header and global styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
       </div>
       <hr className={contactStyles.underline}/>
       <div className={contactStyles.textContainer}>
-        <p className={contactStyles.text}>Let's connect :)</p>
+        <p className={contactStyles.text}>Reach out to chat about anything :)</p>
         <a href="mailto:lia.k.mason@gmail.com">
         <img src={Mail} alt="Email" className={contactStyles.icon}></img>
         </a>
